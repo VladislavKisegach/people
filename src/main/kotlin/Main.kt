3402ff9bb1495f428a6b173391a164cace310e52
vladilav <eaglekibertron@mail.ru>
2023-02-24 11:51:14 +0500

    First commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+   val likes = 99
+   val peaple1 = "людям"
+    val peaple2 = " человеку"
+    val endles = if(likes%2 == 0) println("$likes $peaple1") else println("$likes $peaple2")
+
+    println(endles)
+}
+
