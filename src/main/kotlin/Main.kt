71ca3ddba4489084c608e67715695e9262a2f713
vladilav <eaglekibertron@mail.ru>
2023-02-28 13:19:28 +0500

    New change

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,9 @@
 fun main(args: Array<String>) {
-   val likes = 99
+   val likes = 311
    val peaple1 = "людям"
     val peaple2 = " человеку"
-    val endles = if(likes%2 == 0) println("$likes $peaple1") else println("$likes $peaple2")
+    val endSymbol = likes -1
+    val endles = if(likes%10 == 0) println("$likes $peaple2") else if(endSymbol%10 == 0) println("$likes $peaple1") else println("$likes $peaple2")
 
     println(endles)
 }
